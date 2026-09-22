0aaa806d25b4091b9ba6e7a3b95a10bb69a543f0
Danilo Lana <93826715+danilolana@users.noreply.github.com>
2026-09-22 15:19:00 -0300

    feat - Model, Habitos.java: definição de colunas no banco getters e setters

---
+++ backend/src/main/java/com/maplife/backend/model/Habito.java
@@ -0,0 +1,86 @@
+package com.maplife.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "habito")
+public class Habito {
+
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private int sequenceofdays;
+
+    @Column(nullable = false)
+    private int daytarget;
+
+    public Habito() {
+    }
+
+    public Habito(String name, String description, int sequenceofdays, int daytarget) {
+        this.name = name;
+        this.description = description;
+        this.sequenceofdays = sequenceofdays;
+        this.daytarget = daytarget;
+    }
+
+    public Long  getID(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSequenceofdays() {
+        return sequenceofdays;
+    }
+
+    public int getDaytarget() {
+        return daytarget;
+    }
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setSequenceofdays(int sequenceofdays) {
+		this.sequenceofdays = sequenceofdays;
+	}
+
+	public void setDaytarget(int daytarget) {
+		this.daytarget = daytarget;
+	}
+
+
+    @Override
+    public String toString() {
+        return "Product{id=" + id + ",name='" + name + "', desscription'" + description + 
+        "', sequenceofdays" + sequenceofdays + ", daytarget" + daytarget + ")";
+    }
+
+
+
+}   
+
